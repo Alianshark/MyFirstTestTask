49e557efeefe6c105c72ed5c70095c77bc35f870
EnormouseJohnn <109089482+Alianshark@users.noreply.github.com>
2023-05-11 21:47:12 +0300

    add move ship on key pressed

--- app.js
+++ app.js
@@ -7,6 +7,45 @@ let sprite = PIXI.Sprite.from('./img/rocket.png')
 app.stage.addChild(sprite)
 
 let elapsed = 0.0
-app.ticker.add((delta) => {
+app.ticker.add(gameLoop)
+
+function gameLoop(delta) {
   elapsed += delta
-})
+  if (isArrowLeftPressed) {
+    sprite.x -= 10
+  }
+  if (isArrowRightPressed) {
+    sprite.x += 10
+  }
+}
+
+addKeybordMovement()
+
+let isArrowLeftPressed = false
+let isArrowRightPressed = false
+
+function addKeybordMovement() {
+  window.addEventListener('keydown', handleKeyDown)
+  window.addEventListener('keyup', handleKeyUp)
+}
+
+function handleKeyDown(event) {
+  if (event.key == 'ArrowLeft') {
+    isArrowLeftPressed = true
+    console.log('ArrowLeft pressed', event.key)
+  }
+  if (event.key == 'ArrowRight') {
+    isArrowRightPressed = true
+
+    console.log('ArrowRignt Pressed', event.key)
+  }
+}
+
+function handleKeyUp(event) {
+  if (event.key == 'ArrowLeft') {
+    isArrowLeftPressed = false
+  }
+  if (event.key == 'ArrowRight') {
+    isArrowRightPressed = false
+  }
+}
