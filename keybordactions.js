b70330f12e02e436ef3fdc608b445d9c68445a39
Enormouse <johnnysalvia@gmail.com>
2023-05-17 02:11:00 +0300

    clean up keyboardactions file

--- keybordactions.js
+++ keybordactions.js
@@ -1,6 +1,9 @@
 import { fireBullet } from './bullet.js'
 import { player } from './player.js'
 
+let isArrowRightPressed = false
+let isArrowLeftPressed = false
+
 export function addKeybordMovement(app) {
   window.addEventListener('keydown', handleKeyDown)
   window.addEventListener('keyup', handleKeyUp)
@@ -12,30 +15,28 @@ export function addKeybordMovement(app) {
       isArrowRightPressed = true
     }
     if (event.key == ' ') {
-          fireBullet(app)
-        }
-      }
+      fireBullet(app)
+    }
   }
+}
 
-  let isArrowRightPressed = false
-  let isArrowLeftPressed = false
-    
-  function handleKeyUp(event) {
-    if (event.key == 'ArrowLeft') {
-      isArrowLeftPressed = false
-    }
-    if (event.key == 'ArrowRight') {
-      isArrowRightPressed = false
-    }
+export function movePlayer() {
+  if (isArrowLeftPressed && player.x > 0) {
+    player.x -= 10
   }
+  if (isArrowRightPressed && player.x < 1280 - player.width) {
+    player.x += 10
+  }
+}
+  
+function handleKeyUp(event) {
+  if (event.key == 'ArrowLeft') {
+    isArrowLeftPressed = false
+  }
+  if (event.key == 'ArrowRight') {
+    isArrowRightPressed = false
+  }
+}
 
-  export function movePlayer() {
-    if (isArrowLeftPressed && player.x > 0) {
-      player.x -= 10
-    }
-    if (isArrowRightPressed && player.x < 1280 - player.width) {
-      player.x += 10
-    }
-  }
 
 
