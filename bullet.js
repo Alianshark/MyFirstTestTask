0b6f50b12c0453c8bb318363871817c0df5300e1
Enormouse <johnnysalvia@gmail.com>
2023-05-17 01:55:31 +0300

    refactor bullet from container to graphic object

--- bullet.js
+++ bullet.js
@@ -39,19 +39,14 @@ export function deleteBulletIfLeftScreen (app,bullet) {
 
 
 function createBullet(x,y) {
-  const ract = new PIXI.Graphics()
-  ract.beginFill('yellow')
-  ract.drawRect(0, 0, 20, 40)
-  ract.endFill()
+  const bullet = new PIXI.Graphics()
+  bullet.beginFill(0xffffff)
+  bullet.drawCircle(10, 0, 10)
+  bullet.endFill()
+  bullet.beginFill('yellow')
+  bullet.drawRect(0, 0, 20, 40)
+  bullet.endFill()
 
-  const circle = new PIXI.Graphics()
-  circle.beginFill(0xffffff)
-  circle.drawCircle(10, 0, 10)
-  circle.endFill()
-
-  const bullet = new PIXI.Container()
-  bullet.addChild(circle)
-  bullet.addChild(ract)
   bullet.x = x + player.width / 2 - bullet.width / 2
   bullet.y = y
   return bullet
